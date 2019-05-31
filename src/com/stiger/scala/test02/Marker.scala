c8837191bcea1fdffbb793dd22da2000623eade0
stiger <965553142@qq.com>
2019-05-31 17:53:40 +0800

    05.31

--- src/com/stiger/scala/test02/Marker.scala
+++ src/com/stiger/scala/test02/Marker.scala
@@ -1,17 +1,17 @@
 package com.stiger.scala.test02
 
+/**
+  * 伴生对象实例
+  * @param color
+  */
 // 私有构造方法
 class Marker private(val color:String) {
-
   println("创建" + this)
-
   override def toString(): String = "颜色标记："+ color
-
 }
 
 // 伴生对象，与类名字相同，可以访问类的私有属性和方法
 object Marker{
-
   private val markers: Map[String, Marker] = Map(
     "red" -> new Marker("red"),
     "blue" -> new Marker("blue"),
@@ -26,6 +26,7 @@ object Marker{
     if(markers.contains(color)) markers(color) else null
   }
   def main(args: Array[String]) {
+    println("111111111111111111111")
     println(Marker("red"))
     // 单例函数调用，省略了.(点)符号
     println(Marker getMarker "blue")
---
+++ src/com/stiger/scala/test03/test01.scala
@@ -0,0 +1,20 @@
+package com.stiger.scala.test03
+
+import scala.util.matching.Regex
+
+object test01 {
+
+  def main(args: Array[String]) {
+    val pattern = "Scala".r
+    val str = "Scala is Scalable and cool"
+
+    println(pattern findFirstIn str)
+  }
+  /*def main(args: Array[String]) {
+    val pattern = new Regex("(S|s)cala")  // 首字母可以是大写 S 或小写 s
+    val str = "Scala is scalable and cool"
+
+    println((pattern findAllIn str).mkString(","))   // 使用逗号 , 连接返回结果
+  }*/
+
+}
